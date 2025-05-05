a39d5663ee0e679a0918cf9a18f1661f7c59ac82
AndriyIshchenko <iscenkoa@gmail.com>
2025-05-05 21:20:11 +0300

    feat: update serializers

--- comments/serializers.py
+++ comments/serializers.py
@@ -4,10 +4,36 @@ from comments.models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    user_profile = serializers.SlugRelatedField(
-        many=False, read_only=True, slug_field="nickname"
-    )
+    user = serializers.SlugRelatedField(many=False, read_only=True, slug_field="email")
 
     class Meta:
         model = Comment
-        fields = ("id", "user_profile", "content", "created_at")
+        fields = ("id", "user", "content", "created_at")
+
+
+class CommentListSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(many=False, read_only=True, slug_field="email")
+    replies_count = serializers.SerializerMethodField()
+
+    def get_replies_count(self, obj):
+        return obj.replies.count()  # Count the number of replies
+
+    class Meta:
+        model = Comment
+        fields = (
+            "id",
+            "parent_comment",
+            "user",
+            "content",
+            "created_at",
+            "replies_count",
+        )
+
+
+class CommentDetailSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(many=False, read_only=True, slug_field="email")
+    replies = CommentListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ("id", "parent_comment", "user", "content", "created_at", "replies")
