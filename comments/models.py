f28d36b46e59f525e049c4adeb13096617fb84c2
AndriyIshchenko <iscenkoa@gmail.com>
2025-05-05 21:19:32 +0300

    feat: add user to comment model

--- comments/models.py
+++ comments/models.py
@@ -7,14 +7,14 @@ from django.utils.text import slugify
 
 def comment_image_path(instance, filename) -> str:
     _, extention = os.path.splitext(filename)
-    filename = f"{slugify(instance.user_profile.nickname)}-{uuid.uuid4()}{extention}"
+    filename = f"{slugify(instance.user)}-{uuid.uuid4()}{extention}"
     return os.path.join("uploads/post/", filename)
 
 
 class Comment(models.Model):
-    # user_profile = models.ForeignKey(
-    #     UserProfile, on_delete=models.CASCADE, related_name="posts"
-    # )
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="comments"
+    )
     parent_comment = models.ForeignKey(
         "self", on_delete=models.CASCADE, blank=True, null=True, related_name="replies"
     )
@@ -26,4 +26,4 @@ class Comment(models.Model):
     is_published = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Comment to {self.parent_comment} by {self.user_profile}"
+        return f"Comment to {self.parent_comment} by {self.user}"
