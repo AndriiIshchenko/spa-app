9aea05a7602bdda77d2791e909304bed9478ae38
AndriyIshchenko <iscenkoa@gmail.com>
2025-05-07 19:49:41 +0300

    chore: register models in admin

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from comments.models import Comment, UserProfile
+
+
+admin.site.register(UserProfile)
+admin.site.register(Comment)
